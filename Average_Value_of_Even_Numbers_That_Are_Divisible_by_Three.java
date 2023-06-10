de305b9288ec5925496d7453be81b61d416f8224
Harsh <harsh444577@gmail.com>
2023-06-10 10:54:04 +0530

    Added more java files

---
+++ Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.java
@@ -0,0 +1,34 @@
+public class Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three {
+    public static int averge(int[]nums)
+    {
+        int count = 1;
+        int result = 0;
+        int ans = 0;
+        int harsh = 0;
+        for (int i = 0;i<nums.length; i++)
+        {
+            if (nums[i] %3==0 && nums[i]%2==0)
+            {
+
+                ans = count++;
+                if (ans>1)
+                {
+                    result += nums[i];
+                    harsh =  result/ans;
+
+                }else
+                {
+                    result += nums[i];
+                    harsh =  result;
+                }
+            }
+
+        }
+        return harsh;
+    }
+    public static void main(String[] args) {
+        int[]nums = {1,3,5,7,12,15};
+        System.out.println(averge(nums));
+
+    }
+}
