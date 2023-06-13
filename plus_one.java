f1ecb66fc254bc697c4abb4620a8468a423ccda7
Harsh <harsh444577@gmail.com>
2023-06-13 22:04:09 +0530

    Added more java files

---
+++ plus_one.java
@@ -0,0 +1,22 @@
+public class plus_one {
+    public static int[] plusOne(int[] digits)
+    {
+        int n = digits.length;
+        for(int i = n-1;i >= 0;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        int[] arr = new int[n+1];
+        arr[0]=1;
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int []digits = {1,2,3};
+        System.out.println(plusOne(digits));
+    }
+}
