4e6cd4cc3e02c0267d26521a5a37054682a85f89
Harsh <harsh444577@gmail.com>
2023-06-14 11:58:38 +0530

    Added more java files

--- Valid_Palindrome.java
+++ Valid_Palindrome.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Valid_Palindrome {
+public class Valid_Palindrome {
+    public static boolean isPalindrome(String s)
+    {
+        if (s.isEmpty()) // if s is blank then you should return true
+        {
+            return true;
+        }
+        String str = s.toLowerCase();
+        // remove the non AlphaNumeric character like:- _, $, *
+        str = str.replaceAll("[^a-zA-Z0-9]", "");
+        StringBuilder revStr = new StringBuilder(str);
+        revStr.reverse();
+        String rstr = revStr.toString();
+        if(str.equals(rstr))
+        {
+            return true;
+
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+
+    }
 }
