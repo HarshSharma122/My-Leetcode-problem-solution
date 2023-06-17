e16a0df9083911808b7647fdd4c93b8e7758a106
Harsh <harsh444577@gmail.com>
2023-06-17 16:29:06 +0530

    Added more java files

---
+++ Minimum_Comman_value_in_two_array.java
@@ -0,0 +1,28 @@
+public class Minimum_Comman_value_in_two_array {
+    public static int getComman(int[]nums1, int[]nums2)
+    {
+        for(int i = 0; i<nums1.length; )
+        {
+            for(int j = 0; j<nums2.length; )
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    return nums1[i];
+                }
+                if(nums1[i]<nums2[j])
+                {
+                    ++i;
+                }else
+                {
+                    ++j;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[]nums1 = {1,35,578, 6868, 585858};
+        int[]nums2 = {3363, 37337, 585858};
+        System.out.println(getComman(nums1, nums2));
+    }
+}
