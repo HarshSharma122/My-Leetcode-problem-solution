e01b7fa0129236aae0b9a90361db63ebbab7f5ee
Harsh <harsh444577@gmail.com>
2023-06-18 09:33:54 +0530

    Added more java files

--- Largest_element_of_an_array.java
+++ Largest_element_of_an_array.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Largest_element_of_an_array {
+public class Largest_element_of_an_array {
+    public static int largestEle(int[]nums)
+    {
+        int largest = -1;
+        int index = -1;
+        for (int i =0; i<nums.length; i++)
+        {
+            if (nums[i]>largest)
+            {
+                largest = nums[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int[]nums = {2,4,49,100,4};
+        System.out.println(largestEle(nums));
+
+    }
 }
--- Minimum_Comman_value_in_two_array.java
+++ Minimum_Comman_value_in_two_array.java
@@ -1,7 +1,6 @@
 public class Minimum_Comman_value_in_two_array {
     public static int getComman(int[]nums1, int[]nums2)
     {
-
         int i = 0;
         int j = 0;
         while (i < nums1.length && j < nums2.length) {
