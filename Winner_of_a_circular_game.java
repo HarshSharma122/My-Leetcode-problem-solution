2181e24ab06d2b69b481026c1e554a079260a21a
Harsh <harsh444577@gmail.com>
2023-06-19 20:26:06 +0530

    Added more java files

--- Winner_of_a_circular_game.java
+++ Winner_of_a_circular_game.java
@@ -7,7 +7,6 @@ public class Winner_of_a_circular_game {
             return(findTheWinner(n-1,k)+ k-1)% n+1;
         }
     }
-
     public static void main(String[] args) {
         System.out.println(findTheWinner(5,2));
     }
