7dddd94cfe66e68b932916ad123ac304b660d221
Harsh <harsh444577@gmail.com>
2023-06-19 16:44:51 +0530

    Added more java files

---
+++ Winner_of_a_circular_game.java
@@ -0,0 +1,14 @@
+public class Winner_of_a_circular_game {
+    public static int findTheWinner(int n, int k) {
+        if(n==1){
+            return 1;
+        }else
+        {
+            return(findTheWinner(n-1,k)+ k-1)% n+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findTheWinner(5,2));
+    }
+}
