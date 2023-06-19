cfaaf9348e3a8fc6fd62d01acff14a46615771e2
Harsh <harsh444577@gmail.com>
2023-06-19 15:20:18 +0530

    Added more java files

--- Hamming_distances_is_improved.java
+++ Hamming_distances_is_improved.java
@@ -1,12 +1,17 @@
 public class Hamming_distances_is_improved {
     public static int hammingDistance(int x, int y) {
-//        The Hamming distance between two integers is the number of positions at which the corresponding bits are different
+        //The Hamming distance between two integers is the number of positions at which the corresponding bits are different
         int ans = x^y;
         int result = 0;
         while (ans>0)
 
         {
             result += ans&1;
+            /* this is also work
+             if((ans&1)==1)
+             {
+                 result++;
+             }*/
             ans>>=1;
         }
         return result;
