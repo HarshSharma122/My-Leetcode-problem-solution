cf1ebf3a27038f94136766b2608763d648035666
Harsh <harsh444577@gmail.com>
2023-06-20 23:07:41 +0530

    Added more java files

--- Number_Complement.java
+++ Number_Complement.java
@@ -1,13 +1,17 @@
 public class Number_Complement {
     public static int findComplement(int n)
     {
-      for (long i = 1; i<=n; i<<=1)
-      {
-          n ^= i;
-      }
-       return n;
+//      for (long i = 1; i<=n; i<<=1)
+//      {
+//          n ^= i;
+//      }
+//       return n;
+        // this is another method to solve this problem
+        return ~n & (Integer.highestOneBit(n) - 1);
+
     }
     public static void main(String[] args) {
         System.out.println(findComplement(5));
+
     }
 }
