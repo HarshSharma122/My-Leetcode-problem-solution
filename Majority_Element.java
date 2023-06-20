bd316e4de32b2a4dd52f9408cb944085d6ac35c9
Harsh <harsh444577@gmail.com>
2023-06-20 21:24:09 +0530

    Added more java files

--- Majority_Element.java
+++ Majority_Element.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Majority_Element {
+public class Majority_Element {
+    public static int majorityElement(int[]nums)
+    {
+        int ansIndex = 0;
+        int count = 1;
+        for (int i = 0; i<nums.length; i++)
+        {
+            if (nums[i]==nums[ansIndex])
+            {
+                count++;
+            }else
+
+            {
+                count--;
+            }
+            if (count==0)
+            {
+                ansIndex = i;
+                count = 1;
+
+            }
+        }
+
+        return nums[ansIndex];
+    }
+    public static void main(String[] args) {
+        int[]nums = {3,2,3};
+        System.out.println(majorityElement(nums));
+    }
 }
