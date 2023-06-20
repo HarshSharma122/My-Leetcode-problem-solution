b20ea1fc7c702b6324a13b431d1ee2929695ab9b
Harsh <harsh444577@gmail.com>
2023-06-20 21:08:05 +0530

    Added more java files

---
+++ Missing_number.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Missing_number {
+    public static int missingNumber(int[]nums)
+    {
+        int xor = 0;
+        for(int ans: nums)
+        {
+            xor = xor ^ ans;
+        }
+        for(int i = 0; i<=nums.length;i++)
+        {
+            xor = xor ^ i;
+        }
+        return xor;
+    }
+    public static void main(String[] args) {
+        int[]nums = {0,1};
+        System.out.println(missingNumber(nums));
+
+        System.out.println(1^2);
+    }
+}
