f768147c6cc253174929317597936729e7b1bb81
Harsh <harsh444577@gmail.com>
2023-06-21 10:09:31 +0530

    Added more java files

--- Best_Time_to_Buy_and_Sell_Stock.java
+++ Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,17 +1,27 @@
 public class Best_Time_to_Buy_and_Sell_Stock {
     public static int maxProfit(int[] prices) {
-        int min = prices[0];
-        int max = 0;
-        for(int i = 0; i<prices.length; i++)
+//        int min = prices[0];
+//        int max = 0;
+//        for(int i = 0; i<prices.length; i++)
+//        {
+//            min = Math.min(min, prices[i]);
+//            int profit = prices[i] - min;
+//            max = Math.max(max, profit);
+//        }
+//        return max;
+
+        // 122. Best Time to Buy and Sell Stock II // the solution of that problem
+        // when buy and sell stocks in many times
+        int profit = 0;
+        for (int i = 1; i< prices.length; i++)
         {
-            min = Math.min(min, prices[i]);
-            int profit = prices[i] - min;
-            max = Math.max(max, profit);
+            if (prices[i]>prices[i-1])
+            {
+                profit += (prices[i]-prices[i-1]);
+            }
         }
-        return max;
-
+        return profit;
     }
-
     public static void main(String[] args) {
         int[]nums  = {7,1,5,3,6,4};
         System.out.println(maxProfit(nums));
