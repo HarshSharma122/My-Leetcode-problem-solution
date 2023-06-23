9690914271b16490ddb5a6d9dee78de630018141
Harsh <harsh444577@gmail.com>
2023-06-23 08:54:32 +0530

    Added more java files

---
+++ Count_Primes_Medium_204.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+
+public class Count_Primes_Medium_204 {
+    public static int countPrimes(int n)
+    {
+     boolean[] ans = new boolean[n];
+     int count  = 0 ;
+     boolean flag = true;
+
+     for (int i =2; i<n; i++)
+     {
+         if (ans[i] != flag)
+         {
+             count++;
+             for (int j =2; i*j<n; j++)
+             {
+                 ans[i*j] = flag;
+             }
+         }
+     }
+ return count;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(countPrimes(100));
+    }
+}
