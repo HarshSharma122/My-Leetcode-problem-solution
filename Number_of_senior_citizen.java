745ec420b42d87a4ea3ade260efd5edafab3dd69
Harsh <harsh444577@gmail.com>
2023-06-27 11:36:17 +0530

    Added more java files

--- Number_of_senior_citizen.java
+++ Number_of_senior_citizen.java
@@ -1,17 +1,18 @@
 public class Number_of_senior_citizen {
     public static int senior(String[]nums)
     {
-        int count = 0;
-        for (String s:nums)
-        {
-            int a = s.charAt(11);
+       int count = 0;
+       for (String ans: nums)
+       {
+           int s1 = ans.charAt(11) - '0';
+           int s2 = ans.charAt(12) - '0';
 
-            if (a>6)
-            {
-                count++;
-            }
-        }
-        return count;
+           if ((s1*10) + s2>60)
+           {
+               count++;
+           }
+       }
+       return count;
     }
     public static void main(String[] args) {
         String[]nums = {"7868190130M7522","5303914400F9211","9273338290F4010"};
