8f215104e1143a0cc78703de3fabb737ec0b5977
Harsh <harsh444577@gmail.com>
2023-07-03 10:26:43 +0530

    Added more java files

---
+++ First_Missing_positive.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class First_Missing_positive {
+    public static int missingNum(int[] nums)
+    {
+         int abc = 1;
+         for (int i =0 ; i<nums.length;i++)
+         {
+             if (nums[i]==abc)
+
+             {
+                 abc++;
+             }
+         }
+         return abc;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,7};
+        System.out.println(missingNum(nums));
+    }
+}
